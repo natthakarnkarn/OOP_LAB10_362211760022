3218ea13cb52bc1d28ff13bdeefb097ddd8c3369
nattha <Natthakarnkarn29@gmail.com>
2021-03-16 10:58:23 +0700

    Lab10

--- src/EmployeeDADlmpl.java
+++ src/EmployeeDADlmpl.java
@@ -1,3 +1,14 @@
-public class EmployeeDADlmpl {
+import java.sql.Connection;
+
+public class EmployeeDADlmpl implements EnployeeDAD {
+
+    public static String drivrName = "Org.sqlite.TDBC";
+    public static String url = "jdbc:sqlite:MIT_Company.sqlite";
+    public static Connection conn = null;
+
+
+
+
+
 
 }
